35639ee3240524d9bf7c7b41301ebafd086706d5
hcasten <hcasten@outlook.com>
2023-12-06 18:54:18 -0800

    shuttle template

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use axum::{routing::get, Router};
+use shuttle_runtime::CustomError;
+use sqlx::PgPool;
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+
+#[derive(Clone)]
+struct MyState {
+    pool: PgPool,
+}
+
+#[shuttle_runtime::main]
+async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(CustomError::new)?;
+
+    let state = MyState { pool };
+    let router = Router::new()
+        // .route("/todos", post(add))
+        // .route("/todos/:id", get(retrieve))
+        .with_state(state);
+
+    Ok(router.into())
+}
