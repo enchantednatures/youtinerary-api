044aa2646d4f615967a09183239496c1f59f2b3d
hcasten <hcasten@outlook.com>
2023-12-06 19:55:40 -0800

    add github workflow

--- src/main.rs
+++ src/main.rs
@@ -26,3 +26,55 @@ async fn main(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::Shut
 
     Ok(router.into())
 }
+
+mod models {
+    use serde::{Deserialize, Serialize};
+    use sqlx::FromRow;
+
+    #[derive(FromRow, Serialize, Deserialize)]
+    pub struct User {
+        pub id: i32,
+        pub name: String,
+        pub email: String,
+    }
+
+    #[derive(FromRow, Serialize, Deserialize)]
+    pub struct Itinerary {
+        pub id: i32,
+        pub name: String,
+        pub user_id: i32,
+    }
+
+    #[derive(sqlx::Type, Serialize, Deserialize)]
+    pub enum ItineraryStatus {
+        Draft,
+        Published,
+    }
+
+    #[derive(sqlx::Type, Serialize, Deserialize)]
+    pub enum ItineraryType {
+        Public,
+        Private,
+    }
+
+    #[derive(sqlx::Type, Serialize, Deserialize)]
+    pub enum ItineraryShareType {
+        Owner,
+        Editor,
+        Viewer,
+    }
+
+    #[derive(FromRow, Serialize, Deserialize)]
+    pub struct ItineraryShare {
+        pub id: i32,
+        pub itinerary_id: i32,
+        pub user_id: i32,
+    }
+
+    #[derive(FromRow, Serialize, Deserialize)]
+    pub struct Item {
+        pub id: i32,
+        pub name: String,
+        pub itinerary_id: i32,
+    }
+}
